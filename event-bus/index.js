afd2d7276ffedd399cbc0fd629042a43dde332a9
rohan7051 <rohan.sharma7051@gmail.com>
2026-09-01 23:43:33 +0530

    feat: event bus

---
+++ event-bus/index.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const axios = require("axios");
+const app = express();
+app.use(bodyParser.json());
+
+app.post("/events", async (req, res) => {
+  const event = req.body;
+
+  try {
+    await axios.post("http://localhost:4001/events", event);
+    res.status(200).send({ status: "OK" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error occurred while processing the event");
+  }
+  try {
+    await axios.post("http://localhost:4000/events", event);
+    res.status(200).send({ status: "OK" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error occurred while processing the event");
+  }
+  try {
+    await axios.post("http://localhost:4002/events", event);
+    res.status(200).send({ status: "OK" });
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("Error occurred while processing the event");
+  }
+});
+
+app.listen(4005, () => {
+  console.log("Event bus listening on port 4005");
+});
