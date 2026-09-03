6008069ec9ed5b2b22cc37ca302dccae79cc1424
rohan7051 <rohan.sharma7051@gmail.com>
2026-09-03 22:43:14 +0530

    moderation service

--- comments/index.js
+++ comments/index.js
@@ -17,7 +17,7 @@ app.post("/posts/:id/comments", async (req, res) => {
   const { content } = req.body;
 
   const comments = commentsByPostId[req.params.id] || [];
-  comments.push({ id: commentId, content });
+  comments.push({ id: commentId, content, status: "pending" });
   commentsByPostId[req.params.id] = comments;
 
   await axios.post("http://localhost:4005/events", {
@@ -26,6 +26,7 @@ app.post("/posts/:id/comments", async (req, res) => {
       id: commentId,
       content,
       postId: req.params.id,
+      status: "pending",
     },
   });
 
@@ -34,6 +35,25 @@ app.post("/posts/:id/comments", async (req, res) => {
 
 app.post("/events", async (req, res) => {
   console.log("Received Event:", req.body.type);
+
+  const { type, data } = req.body;
+
+  if (type === "CommentModerated") {
+    const { postId, id, status, content } = data;
+    const comments = commentsByPostId[postId];
+    const comment = comments.find((comment) => comment.id === id);
+    comment.status = status;
+
+    await axios.post("http://localhost:4005/events", {
+      type: "CommentUpdated",
+      data: {
+        id,
+        postId,
+        content,
+        status,
+      },
+    });
+  }
   res.send({});
 });
 
--- event-bus/index.js
+++ event-bus/index.js
@@ -4,20 +4,29 @@ const axios = require("axios");
 const app = express();
 app.use(bodyParser.json());
 
+const events = [];
+
 app.post("/events", async (req, res) => {
   const event = req.body;
+
+  events.push(event);
 
   try {
     await Promise.all([
       axios.post("http://localhost:4000/events", event),
       axios.post("http://localhost:4001/events", event),
       axios.post("http://localhost:4002/events", event),
+      axios.post("http://localhost:4003/events", event),
     ]);
     res.status(200).send({ status: "OK" });
   } catch (err) {
     console.error(err);
     res.status(500).send("Error occurred while processing the event");
   }
+});
+
+app.get("/events", (req, res) => {
+  res.send(events);
 });
 
 app.listen(4005, () => {
