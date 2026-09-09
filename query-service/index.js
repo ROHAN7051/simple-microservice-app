b39ef6ec113e14684ab91e22addb1578d25572b0
rohan7051 <rohan.sharma7051@gmail.com>
2026-09-09 23:32:11 +0530

    chore: devops

--- query-service/index.js
+++ query-service/index.js
@@ -43,7 +43,7 @@ app.post("/events", (req, res) => {
 app.listen(4002, async () => {
   console.log("Query service is running on port 4002");
   try {
-    const res = await axios.get("http://localhost:4005/events");
+    const res = await axios.get("http://event-bus-srv:4005/events");
     res.data.forEach((event) => {
       handleEvent(event.type, event.data);
     });
